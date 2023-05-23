2383f7f4df055376a2977e11036bb2c6cbce148a
MateoGJ <mateo.garciajuarez12@gmail.com>
2023-05-22 21:19:23 -0300

    ,

--- CreacionBD.sql
+++ CreacionBD.sql
@@ -1,4 +1,3 @@
-create database BD_Ejemplo_IT; 
-use bd_ejemplo_it;
+ create database BD_Ejemplo_IT; 
 create table persona(DNI int, Nombre varchar(30),
- Direccion varchar(30), Telefono int);	
+ Direccion varchar(30), Telefono int(11));	
