22121c9ecf3f00d94887848353a4f508896cdbe2
violeta <evangelina1977@gmail.com>
2023-06-09 11:04:42 -0300

    arregle algunos cambios

--- Viole1.py
+++ Viole1.py
@@ -1,8 +1,8 @@
-import mysqyl.conector
+import mysql.conector
 
 from bd_big_bread import bd_big_bread
-class Conectar ():
-    def _init_ (auto) -> None:
+class Conectar():
+    def __init__(self) -> None:
         try:
             self.conexion = mysql.connector.connet(
                 anfitrion = '127.0.0.1',
@@ -13,87 +13,41 @@ class Conectar ():
                 )
             if self.conexion.is_connected():
             
-             
              print ("LA CONEXION FUE EXITOSA") 
-                
-                
+                   
         except:
-            print ("NO SE PUDO CONECTAR A LA BASE DE DATOS")
-                    
-        finally:
-            if self.conexion.is_connected():
-                self.conexion.close()
-                print("LA CONEXION FUE CERRADA")
+            print(
+"NO SE PUDO CONECTAR A LA BASE DE DATOS")
                             
-    
-                
-            
-    def  Insertar_Producto (self, iD_Producto, nombre_Producto, stock, precio, unidad_de_medida, iD_Receta):
-        if self.conexion.is_connected():
-            try:
-                cursor = self.conexion.cursor()
-                sentenciaSQL = "INSERT INTO Productos VALUES(%s,%s,%s,%s,%s,%s)"
-                datos = (iD_Producto, nombre_Producto, stock, precio, unidad_de_medida, iD_Receta)
-                cursor.execute(sentenciaSQL, datos)
-                cursor.conexion.commit()
-                cursor.conexion.close()   
-            except mysql.connector.Error as descripcionDelError:
-                print("¡Hubo un error al intentar conectar la Base de Datos", descripcionDelError)
+def Insertar_Producto(self.producto):
+    if self.conexion.is_connected():
+        try:
+            cursor = self.conexion.cursor()
+            sentenciaSQL = "INSERT INTO Productos VALUES(%s,%s,%s,%s,%s,%s,null)"
+            data = (producto.getiD_Producto(),
+                    producto.getnombre_Producto(),
+                    producto.getstock(),
+                    producto.getprecio(),
+                    producto.unidad_de_medida(),
+                    producto.getiD_Receta)
+            cursor.execute(sentenciaSQL,data)
+            cursor.Conexion.commit()
+            cursor.Conexion.close()
+            print("productoinsertado correctamente")
+               
+        except mysql.connector.Error as descripcionDelError:
+            print("¡Hubo un error al intentar conectar la Base de Datos", descripcionDelError)
 
-    def  Listado_De_Productos ( self ):
-            si  uno mismo . conexión _está_conectado ():
-                intentar :
-                    cursor  =  uno mismo . conexión _cursor ()
-                    sentenciaSQL  =  "SELECCIONAR * DE Productos"
-                    cursor _ ejecutar ( sentenciaSQL )
-                    resultados =  cursor . buscar ()
-                    cursor _ Conexión _ cerca ()
-                    devolver resultados
+def  Listado_De_Productos(self):
+        if self.conexión.is_connected():
+        try:
+            cursor  =  self.conexión.cursor() 
+            sentenciaSQL  =  "SELEC * from Productos"
+            cursor.execute(sentenciaSQL)
+            resultados=  cursor.fetchall()
+            cursor.Conexion.close()
+            return resultados 
+        except mysql:connector.Error as descripcionDelError:
+    print("¡hubo un erroral intentar conectar a la Base de datos", decripcionDelError
+    )
 
-definitivamente  Insertar_Producto ( self , producto ):
-    si uno mismo . conexión _ está_conectado ():
-        prueba :
-            cursor  = uno mismo . conexión _ cursor ()
-            sentenciaSQL  =  "INSERT INTO valores de productos (null,%s,%s,%s,%s,null)"
-            datos  = ( producto . getiD_Producto () ),
-                    producto _ getnombre_Producto (),
-                    producto _ obtener stock (),
-                    producto _ obtener precio (),
-                    producto _ getunidad_de_medida (),
-                    producto _ getiD_Receta ()
-                    )
-
-            excepto  mysql . conector _ Error  como  descripcionDelError :
-                print ( "Hubo un error al intentar conctar la Base de Datos" , descripcionDelError )
-            cursor _ ejecutar ( sentenciaSQL , datos )
-            cursor _ Conexión _ cometer ()
-            cursor _ Conexión _ cerrar ()   
-            print ( "Producto insertado correctamente" )
-
-        excepto  mysql . conector _ Error  como  descripcionDelError :
-            print ( "Hubo un error al intentar conctar la Base de Datos" , descripcionDelError )
-
-
-    def  Listado_De_Productos ( self ):
-        si  uno mismo . conexión _está_conectado ():
-            intentar :
-                cursor  =  uno mismo . conexión _cursor ()
-                sentenciaSQL  =  "SELECCIONAR * DE Productos"
-                cursor _ ejecutar ( sentenciaSQL )
-                resultados =  cursor . buscar ()
-                cursor _ Conexión _ cerca ()
-                devolver resultados
-def  Listado_De_Productos ( self ):
-    si  uno mismo . conexión _está_conectado ():
-        intentar :
-            cursor  =  uno mismo . conexión _cursor ()
-            sentenciaSQL  =  "SELECCIONAR * DE Productos"
-            cursor _ ejecutar ( sentenciaSQL )
-            resultados =  cursor .buscar ()
-            cursor _ Conexión _cerrar ()
-            devolver  resultados
-
-            excepto  mysql . conector _ Error  como  descripcionDelError :
-                print ( "Hubo un error al intentar conctar la Base de Datos" , descripcionDelError )
-        excepto  mysql . conector _ Error  como  descripcionDelError :
-            print ( "Hubo un error al intentar conctar la Base de Datos" , descripcionDelError )
